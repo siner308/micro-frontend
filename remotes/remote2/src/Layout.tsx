7dc623c5bc19e1ba5f1c1c63fe4e4b91b39c41c5
jeonghyun.an <jeonghyun.an@devsisters.com>
2023-07-04 10:14:30 +0900

    make host multiple

--- remotes/remote2/src/Layout.tsx
+++ remotes/remote2/src/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-type LayoutProps = {
+type Props = {
   children?: React.ReactNode;
 };
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<Props> = (props: Props) => {
+  const { children } = props;
   return <div style={{backgroundColor: 'aliceblue'}}>
     <div style={{fontSize: '32px'}}>this is new version of remote2 component</div>
     {children}
